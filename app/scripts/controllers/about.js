3bb4fb91efd4c85caa142b03874e9fa199d096c5
acatella <aficclark@gmail.com>
2016-04-10 18:00:55 -0700

    bootstrapped yeoman angular project, configured grunt-sass

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngPracticeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngPracticeApp
+ */
+angular.module('ngPracticeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngPracticeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngPracticeApp
+ */
+angular.module('ngPracticeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
